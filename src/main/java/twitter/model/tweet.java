444ec3c427b0ae7af7df4fa0a8103d5864950e54
Ryan <ryan.w.boehm@gmail.com>
2024-07-04 21:24:10 -0500

    set up model for tweet:

--- src/main/java/twitter/model/tweet.java
+++ src/main/java/twitter/model/tweet.java
@@ -1,5 +1,38 @@
 package twitter.model;
+import java.time.LocalDateTime;
 
-public class tweet {
-    
+public class Tweet {
+    private String text;
+    private LocalDateTime createdDate;
+    private Integer authorId;
+
+    public Tweet(String text, LocalDateTime createdDate, Integer authorId) {
+        this.text = text;
+        this.createdDate = createdDate;
+        this.authorId = authorId;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public LocalDateTime getCreatedDate() {
+        return createdDate;
+    }
+
+    public Integer getAuthorId() {
+        return authorId;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public void setCreatedDate(LocalDateTime createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public void setAuthorId(Integer authorId) {
+        this.authorId = authorId;
+    }
 }
